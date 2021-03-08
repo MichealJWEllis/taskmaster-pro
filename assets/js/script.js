65c25feddd8715785e6b120997094bfa834089dc
Micheal Ellis <mellis.eriecon@gmail.com>
2021-03-07 20:52:22 -0500

    finished lesson 5.1.8 of module 5 / code working

--- assets/js/script.js
+++ assets/js/script.js
@@ -120,6 +120,58 @@ $("#task-form-modal .btn-primary").click(function () {
   }
 });
 
+// due date was clicked
+$(".list-group").on("click", "span", function () {
+  //get current text
+  var date = $(this)
+    .text()
+    .trim();
+
+  // create new input element
+  var dateInput = $("<input>")
+    .attr("type", "text")
+    .addClass("form-control")
+    .val(date);
+
+  // swap out elements
+  $(this).replaceWith(dateInput);
+
+  // automatically focus on new element
+  dateInput.trigger("focus");
+
+})
+
+// value of due date was changed
+$(".list-group").on("blur", "input[type='text']", function () {
+  // get current text
+  var date = $(this)
+    .val()
+    .trim();
+
+  // get the parent ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .arr("id")
+    .replace("list-", "");
+
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // update task in array and re-save to localstorage
+  tasks[status][index].date = date;
+  saveTasks();
+
+  // recreate span element with boostrap classes
+  var taskSpan = $("<span>")
+    .addClass("badge badge-primary badge-pill")
+    .text(date);
+
+  // replace input with span element
+  $(this).replaceWith(taskSpan);
+})
+
 // remove all tasks
 $("#remove-tasks").on("click", function () {
   for (var key in tasks) {
@@ -128,6 +180,7 @@ $("#remove-tasks").on("click", function () {
   }
   saveTasks();
 });
+
 
 // load tasks for the first time
 loadTasks();
